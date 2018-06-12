76fc1e851f72f3567cb1310897fb10d73609b30e
Georgina <geogarrido@falabella.cl>
2018-06-12 12:37:32 -0400

    prueba sonar

--- src/main/java/com/rgfp/psd/logbook/domain/Note.java
+++ src/main/java/com/rgfp/psd/logbook/domain/Note.java
@@ -15,12 +15,54 @@ import java.time.LocalDateTime;
 @Table(name = "notes")
 public class Note {
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     private String title;
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
     private LocalDateTime timestamp;
     @Column(length=10000)
     private String content;
-
+    private String Summary;
+    public String getSummary() {
+  if (this.content.length()> 240)  {
+      Summary = this.content.substring(0,240);
+  } else {
+      Summary = this.content;
+  }
+        return Summary;
+    }
 }
--- src/test/java/com/rgfp/psd/logbook/domain/NoteTest.java
+++ src/test/java/com/rgfp/psd/logbook/domain/NoteTest.java
@@ -5,5 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class NoteTest {
+    @Test
+    public void tieneResumenNota() {
+
+        // Arrange
+        String nota = "esta es una nota que tiene mas de 240 caracteres, pero solo voy a mostrar los primeros 240, " +
+        "porque eso me estan pidiendo y quiero saber si me sale bien y si resulta sere my feliz como Paola" +
+        " y seguramente ella se reira de mi, porque hare mi baile de la victoria y se va a sorprender";
+
+        // Act
+        Note note = new Note();
+        note.setContent(nota);
+        String result = note.getSummary();
+        Integer largo = result.length();
+        // Assert
+        assertEquals(new Integer(240), largo);
+
+
+    }
 
 }
